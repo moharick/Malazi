4d0268c02324a9d581ec45eb5cb98f46b47d0511
moharick <moharick@gmail.com>
2019-11-01 09:54:39 +0300

    Create the user login view

--- book/views.py
+++ book/views.py
@@ -34,3 +34,27 @@ def register(request):
         form = UserForm()
         args = {'form': form}
         return render(request, 'reg_form.html', args)
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password'])
+            if user is not None:
+                if user.is_warden:
+                    return HttpResponse('Invalid Login')
+                if user.is_active:
+                    login(request, user)
+                    student = request.user.student
+                    return render(request, 'profile.html', {'student': student})
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid Login')
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
