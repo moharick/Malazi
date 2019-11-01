d29c6ef1b58ee5ce6b59b335bba820e22b48c268
moharick <moharick@gmail.com>
2019-11-01 09:07:51 +0300

    create other model classes.

--- book/models.py
+++ book/models.py
@@ -67,3 +67,27 @@ class Hostel(models.Model):
     def __str__(self):
         return self.name
 
+
+class Course(models.Model):
+    # if a student has enrollment number iit2017001 then the course code is iit2017
+    code = models.CharField(max_length=100, default=None)
+    room_choice = [('S', 'Single Occupancy'), ('D', 'Double Occupancy'), ('P', 'Reserved for Research Scholars'), ('B', 'Both Single and Double Occupancy')]
+    room_type = models.CharField(choices=room_choice, max_length=1, default='D')
+
+    def __str__(self):
+        return self.code
+
+class Warden(models.Model):
+    user = models.OneToOneField(
+        User,
+        default=None,
+        null=True,
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    hostel = models.ForeignKey('Hostel',
+        default=None,
+        null=True,
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
