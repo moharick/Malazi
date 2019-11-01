98ed2d7f931d3fb1315e58672202974388056dec
moharick <moharick@gmail.com>
2019-11-01 09:04:27 +0300

    create room  model class.

--- book/models.py
+++ book/models.py
@@ -40,3 +40,14 @@ class Student(models.Model):
 
     def __str__(self):
         return self.enrollment_no
+
+class Room(models.Model):
+    room_choice = [('S', 'Single Occupancy'), ('D', 'Double Occupancy'), ('P', 'Reserved for Research Scholars'),('B', 'Both Single and Double Occupancy')]
+    no = models.CharField(max_length=5)
+    name = models.CharField(max_length=10)
+    room_type = models.CharField(choices=room_choice, max_length=1, default=None)
+    vacant = models.BooleanField(default=False)
+    hostel = models.ForeignKey('Hostel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
